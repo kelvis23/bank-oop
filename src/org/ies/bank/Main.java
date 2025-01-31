ac7970c6fd8e4a2252b3dcda339d3498d60d314f
kelvis <jesuscuestas116@gmail.com>
2025-01-31 14:12:01 +0100

    radom 2

--- src/org/ies/bank/Main.java
+++ src/org/ies/bank/Main.java
@@ -1,18 +1,17 @@
 package org.ies.bank;
 
+import org.ies.bank.componentes.Reader;
+import org.ies.bank.componentes.random.RandomAccountReader;
+import org.ies.bank.model.Account;
 import org.ies.bank.model.Banck;
 
+import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner =new Scanner(System.in);
-        BanckReader banckReader= new BanckReader() {
-            @Override
-            public Banck read() {
-                return null;
-            }
-        };
+        Random random =new Random();
+        Reader<Account> randomAccountReader = new RandomAccountReader();
 
     }
 }
