20d144f53f1ff03ca62958628900be421a2f407f
kelvis <jesuscuestas116@gmail.com>
2025-02-21 14:20:44 +0100

    radom 3

--- src/org/ies/bank/Main.java
+++ src/org/ies/bank/Main.java
@@ -11,7 +11,6 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Random random =new Random();
-        Reader<Account> randomAccountReader = new RandomAccountReader();
 
     }
 }
